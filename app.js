612c3db2dfae04a963dfb2c4891ed8671d6e7d44
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-09-13 14:03:11 +0500

    Interface updated with respect to the Plant Database

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-d3.csv("movies.csv").then(function (data) {
-    // console.log(data);
-  
-    var movies = data;
+d3.csv("Database_plant.csv").then(function (data) {
+    
+    var plant_details = data;
   
     var button = d3.select("#button");
   
@@ -23,21 +22,24 @@ d3.csv("movies.csv").then(function (data) {
         d3.select("p").classed('noresults2', true).html("<center><strong>Please try using more than 5 characters to avoid too many results!</strong>")
         inputValue = "Something to give no results"
       }
-      var filteredData = movies.filter(movies => movies.actors.toLowerCase().trim().includes(inputValue));
+      console.log(plant_details)
+      var filteredData = plant_details.filter(plant_details => plant_details.common_name.toLowerCase().trim().includes(inputValue));
       // console.log(filteredData.length)
       if (filteredData.length === 0 && inputValue !== "Something to give no results"){
         d3.select("p").classed('noresults', true).html("<center><strong>No results. Please check your spelling!</strong>")
       }
-      output = _.sortBy(filteredData, 'avg_vote').reverse()
-  
+      
+    //   output = _.sortBy(filteredData, 'avg_vote').reverse()
+         output = filteredData
       for (var i = 0; i < filteredData.length; i++) {
         // console.log(output[i]['original_title'])
         // console.log(output[i]['avg_vote'])
         // d3.select("tbody>tr>td").text(output[i]['original_title']);
         d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
-        + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>") }
+        + "</td>" +"<td>" +(output[i]['bloom_time'])+"</td>" +"<td>" +(output[i]['suitable_site_conditions'])+"</td>"  +"<td>" +(output[i]['soil_type'])+"</td>"+"<td>" +(output[i]['climate_appropriate_plants'])+"</td>") }
     };
     window.resizeTo(screen.width,screen.height)
   
   
   });
+
